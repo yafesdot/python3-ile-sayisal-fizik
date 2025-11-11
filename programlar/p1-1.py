a2f56779cbe70e8c074881c94eb9106f3502ef0c
Mustafa Yafes Akbaş <62028911+yafesdot@users.noreply.github.com>
2025-11-11 23:04:21 +0300

    alt alta göstermenin daha iyi olacağını düşünmeye başladım

--- programlar/p1-1.py
+++ programlar/p1-1.py
@@ -5,8 +5,10 @@ def f(x):
 
 a = 1.0
 h = 0.1
-print("h degeri       turev degeri hata payi")
+print("h degeri          turev degeri    hata payi")
 for i in range(14):
     f1 = (f(a + h) - f(a)) / h
-    print(f"{h:.12f} {f1:.10f} {abs(math.cos(a) - f1):.10f}") # Hata payini mutlak degerini alarak gosterdim
+    print(f"{h:.12f}\
+    {f1:.10f}\
+    {abs(math.cos(a) - f1):.10f}") # Hata payini mutlak degerini alarak gosterdim
     h = h / 10.0
